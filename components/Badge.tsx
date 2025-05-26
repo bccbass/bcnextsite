a9a51d71a54bb6045595cbbdf8957f50fb23ef9a
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2025-05-26 14:58:28 +1000

    bg gradient

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Badge = ({color}:{color?: string}) => {
   return (
     <Link href={"/"} className={`font-feature uppercase col-centered w-fit  gap-2 ` }>
-          <h1 className={`${color} text-outline-accent text-2xl sm:text-4xl md:text-4xl lg:text-5xl -tracking-[.15rem] font-semibold `}>
+          <h1 className={`${color} badge-title text-2xl sm:text-4xl md:text-4xl lg:text-5xl -tracking-[.15rem] font-semibold `}>
             Benjamin Campbell
           </h1>
           <h2 className={`${ color } text-sm sm:text-xl md:text-lg md:tracking-[.12rem]`}>
