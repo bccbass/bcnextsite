0d57421c184d937756514af0f38a1566265db66b
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2025-05-26 12:23:04 +1000

    updated section title, sync

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -9,7 +9,7 @@ const About = ({ briefBiography }: { briefBiography: PortableTextBlock[] | strin
     <div className="flex flex-col gap-16 section pt-24">
       <div className="  flex flex-col md:flex-row gap-6 md:gap-16 lg:gap-20">
         <h2 className="section-title">about</h2>
-        <div className="prose prose-lg pretty ">
+        <div className="prose prose-lg pretty">
           
           {typeof briefBiography !== 'string' ? <PortableText value={briefBiography} /> : <div>{briefBiography}</div> }
         </div>
