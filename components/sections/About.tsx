420ebda71dd7e24c57aec1f775e78b72a7dfb7a2
bccbass <109984363+bccbass@users.noreply.github.com>
2025-06-03 15:27:54 +1000

    linting, deleted unused compon.

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/link";
+// import Link from "next/link";
 import { PortableText } from "@portabletext/react";
 
 import type { PortableTextBlock } from "@sanity/types";
--- components/sections/Explore.tsx
+++ components/sections/Explore.tsx
@@ -10,7 +10,7 @@ const Explore = async () => {
 
     return (
     <div className='pt-12 pb-30 md:pb-48 flex flex-wrap justify-center gap-16 md:gap-24 xl:gap-32'>
-      {  sections.map((section, i) => <FeatureCard key={section._id} title={section.title} slug={section.slug.current} image={section.imageUrl} description={section.description} />
+      {  sections.map((section) => <FeatureCard key={section._id} title={section.title} slug={section.slug.current} image={section.imageUrl} description={section.description} />
         )
 }
     </div>
