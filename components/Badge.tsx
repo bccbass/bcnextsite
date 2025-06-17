91c407398da70bfd7f3ebac37d4fac4cd4c9d042
bccbass <109984363+bccbass@users.noreply.github.com>
2025-06-17 15:36:06 +1000

    testing

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -5,10 +5,10 @@ import Link from 'next/link'
 const Badge = () => {
   return (
     <Link href={"/"} className={`uppercase col-centered w-fit  gap-2 overflow-hidden p-4` }>
-          <h1 className='text-center badge-title text-5xl sm:text-7xl  xl:text-[5.2rem] -tracking-[.15rem]'>
+          <h1 className='text-center  text-5xl sm:text-7xl  xl:text-[5.2rem] -tracking-[.15rem]'>
             Benjamin
           </h1>
-          <h1 className='text-center badge-title text-5xl sm:text-7xl  xl:text-[5.2rem] -tracking-[.15rem]'>
+          <h1 className='text-center  text-5xl sm:text-7xl  xl:text-[5.2rem] -tracking-[.15rem]'>
              Campbell
           </h1>
           <h2 className={`text-center text-md sm:text-xl font-feature font-bold md:text-lg md:tracking-[.12rem]`}>
