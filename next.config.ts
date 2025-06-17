c33764e68ba082f20268910a3f741d5412108f70
bccbass <109984363+bccbass@users.noreply.github.com>
2025-06-17 16:43:44 +1000

    config

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,9 @@ const nextConfig: NextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
-  experimental: {
-    optimizeCss: true, // Keep this if you added it
-  },
+  // experimental: {
+  //   optimizeCss: true, // Keep this if you added it
+  // },
   images: {
     formats: ["image/webp", "image/avif"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
