7c9ac064313effe3a36780b6841caae21705f4f4
bccbass <109984363+bccbass@users.noreply.github.com>
2025-06-17 16:32:33 +1000

    badge refactor for performance

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -5,10 +5,10 @@ const badgeStyle = 'text-center  text-5xl sm:text-7xl  xl:text-[5.2rem] -trackin
 const Badge = () => {
   return (
     <Link href={"/"} className={`uppercase col-centered w-fit  gap-2 overflow-hidden p-4` }>
-          <h1 className={badgeStyle}>
+          <h1 className='text-center  text-5xl sm:text-7xl  xl:text-[5.2rem] -tracking-[.15rem] drop-shadow-[3px_3px_1px_rgba(37,150,190)] text-accent/80 font-bold  uppercase'>
             Benjamin
           </h1>
-          <h1 className={badgeStyle}>
+          <h1 className='text-center  text-5xl sm:text-7xl  xl:text-[5.2rem] -tracking-[.15rem] drop-shadow-[3px_3px_1px_rgba(37,150,190)] text-accent/80 font-bold  uppercase'>
              Campbell
           </h1>
           <h2 className={`text-center text-md sm:text-xl font-feature font-bold md:text-lg md:tracking-[.12rem]`}>
