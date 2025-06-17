4bce49e04a7df2bff345f21559ec5a6255a62605
bccbass <109984363+bccbass@users.noreply.github.com>
2025-06-17 14:25:38 +1000

    removed unused prop from badge element

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Link from 'next/link'
 
 const badgeStyle = 'text-center badge-title text-5xl sm:text-6xl  sm:text-7xl  xl:text-[5.2rem] -tracking-[.15rem]'
-const Badge = ({color}:{color?: string}) => {
+const Badge = () => {
   return (
     <Link href={"/"} className={`uppercase col-centered w-fit  gap-2 overflow-hidden p-4` }>
-          <h1 className={`${color} ${badgeStyle}`}>
+          <h1 className={`${badgeStyle}`}>
             Benjamin
           </h1>
-          <h1 className={`${color} ${badgeStyle}`}>
+          <h1 className={`${badgeStyle}`}>
              Campbell
           </h1>
-          <h2 className={`${ color } text-center text-md sm:text-xl font-feature font-bold md:text-lg md:tracking-[.12rem]`}>
+          <h2 className={`text-center text-md sm:text-xl font-feature font-bold md:text-lg md:tracking-[.12rem]`}>
             Bassist | Composer | Educator
           </h2>
         </Link>
