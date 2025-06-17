ec45fd2b61401a235abb7c29bbcfc124c3f13c7b
bccbass <109984363+bccbass@users.noreply.github.com>
2025-06-17 15:42:23 +1000

    refactored badge styles

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 
-// const badgeStyle = 'text-center badge-title text-5xl sm:text-6xl  sm:text-7xl  xl:text-[5.2rem] -tracking-[.15rem]'
+const badgeStyle = 'text-center  text-5xl sm:text-7xl  xl:text-[5.2rem] -tracking-[.15rem] drop-shadow-[3px_3px_1px_rgba(37,150,190)] text-accent/80 font-bold  uppercase'
 const Badge = () => {
   return (
     <Link href={"/"} className={`uppercase col-centered w-fit  gap-2 overflow-hidden p-4` }>
-          <h1 className='text-center  text-5xl sm:text-7xl  xl:text-[5.2rem] -tracking-[.15rem]'>
+          <h1 className={badgeStyle}>
             Benjamin
           </h1>
-          <h1 className='text-center  text-5xl sm:text-7xl  xl:text-[5.2rem] -tracking-[.15rem]'>
+          <h1 className={badgeStyle}>
              Campbell
           </h1>
           <h2 className={`text-center text-md sm:text-xl font-feature font-bold md:text-lg md:tracking-[.12rem]`}>
