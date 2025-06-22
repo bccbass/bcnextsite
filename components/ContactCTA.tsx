d8c537786f81e43e80a04be02f2def9a33f181ac
bccbass <109984363+bccbass@users.noreply.github.com>
2025-06-22 17:31:43 +1000

    cta detail

--- components/ContactCTA.tsx
+++ components/ContactCTA.tsx
@@ -4,7 +4,7 @@ import Contact from "@/components/Contact";
 
 const ContactCTA = () => {
   return (
-    <div className="my-20 md:my-32 w-screen md:w-md bg-secondary sm:outline-4 py-20 md:py-12 px-12 outline-neutral-200 shadow-2xl md:rounded-md flex flex-col items-center gap-6">
+    <div className="my-20 md:my-32 w-screen md:w-md bg-secondary md:outline-4 py-20 md:py-12 px-12 outline-neutral-200 shadow-2xl md:rounded-md flex flex-col items-center gap-6">
       <h3 className="section-title">Oh, hey!</h3>
       <p className="text-xl font-semibold text-center">
         If you have found anything in these pages interesting, would like to
